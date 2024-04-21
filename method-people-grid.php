941925b1a66eef2108cc65af057d1b8186524add
Rob Clark <rob@robclark.io>
2024-04-21 16:16:20 -0500

    Added ordering options, made posttype hierarchical, and added social link support

--- method-people-grid.php
+++ method-people-grid.php
@@ -3,7 +3,7 @@
  * Plugin Name: Method People Grid
  * Plugin URI: https://github.com/pixelwatt/method-people-grid
  * Description: This plugin adds a versitile shortcode for displaying grids of people, with AJAX-powered modals.
- * Version: 0.9.1
+ * Version: 0.9.2
  * Author: Rob Clark
  * Author URI: https://robclark.io
  */
@@ -113,7 +113,7 @@ function method_people_grid_post_type_init() {
 		'query_var'          => false,
 		'capability_type'    => 'post',
 		'has_archive'        => false,
-		'hierarchical'       => false,
+		'hierarchical'       => true,
 		'menu_position' 	 => 5,
 		'menu_icon'			 => 'dashicons-megaphone',
 		'supports'           => array( 'title' , 'editor', 'thumbnail' )
@@ -165,8 +165,11 @@ function method_people_grid_add_shortcode( $atts, $content, $shortcode_tag ) {
 	$atts = shortcode_atts( array(
 		'id' => '',
 		'headline' => '',
+		'orderby' => 'menu_order',
+		'order' => 'ASC',
 		'htag' => $layout->get_option( 'shortcode_htag', 'h2' ),
 		'modals' => ( 'yes' == $layout->get_option( 'shortcode_modals', 'yes' ) ? true : false ),
+		'social' => ( 'yes' == $layout->get_option( 'shortcode_social', 'no' ) ? true : false ),
 		'spacing' => $layout->get_option( 'grid_spacing', '3' ),
 		'people_per_row' => $layout->get_option( 'people_per_row', '4' ),
 	), $atts, 'peoplegroup' );
@@ -175,7 +178,8 @@ function method_people_grid_add_shortcode( $atts, $content, $shortcode_tag ) {
 		$pids = explode( ',', $atts['id'] );
 		$pargs = array(
 			'post_type' => 'method_people',
-			'order' => 'menu_order',
+			'orderby' => $atts['orderby'],
+			'order' => $atts['order'],
 			'numberposts' => -1,
 			'fields' => 'ids',
 			'tax_query' => array(
@@ -221,6 +225,9 @@ function method_people_grid_add_shortcode( $atts, $content, $shortcode_tag ) {
 						';
 						if ( ( $layout->get_loaded_meta( '_method_people_title' ) ) || ( $layout->get_loaded_meta( '_method_people_org' ) ) ) {
 							$shou .= '<p class="method-peoplegroup-item-org">' . $layout->get_loaded_headline( '_method_people_title', '', '' ) . ( $layout->get_loaded_meta( '_method_people_title' ) ? ( $layout->get_loaded_meta( '_method_people_org' ) ? ', <br>' : '' ) : '' ) . $layout->get_loaded_headline( '_method_people_org', '', '' ) . '</p>';
+						}
+						if ( true === $atts['social'] ) {
+							$shou .= method_people_grid_build_social_icons( 'method-people-group-social', 24, $layout->get_serialized_loaded_meta( '_method_people_social_accounts' ) );
 						}
 						$shou .= '
 								</div>
@@ -290,6 +297,65 @@ function method_people_grid_add_shortcode( $atts, $content, $shortcode_tag ) {
 
 add_shortcode( 'peoplegroup', 'method_people_grid_add_shortcode' );
 
+
+function method_people_grid_build_social_icons( $class = 's-ics', $icon_size = 16, $data ) {
+		$output = '';
+
+		$social_links = $data;
+		if ( ! empty( $social_links ) ) {
+			if ( is_array( $social_links ) ) {
+				if ( method_people_grid_check_array( $social_links, 'service' ) ) {
+					$output .= '<ul class="' . $class . '">';
+
+					foreach ( $social_links as $link ) {
+						$service = $link['service'];
+
+						switch ( $service ) {
+							case 'facebook':
+								$icon = '<svg xmlns="http://www.w3.org/2000/svg" width="' . $icon_size . '" height="' . $icon_size . '" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16"><path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/></svg>';
+								break;
+							case 'twitter':
+								$icon = '<svg xmlns="http://www.w3.org/2000/svg" width="' . $icon_size . '" height="' . $icon_size . '" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16"><path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865l8.875 11.633Z"/></svg>';
+								break;
+							case 'linkedin':
+								$icon = '<svg xmlns="http://www.w3.org/2000/svg" width="' . $icon_size . '" height="' . $icon_size . '" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16"><path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401m-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4"/></svg>';
+								break;
+							case 'instagram':
+								$icon = '<svg xmlns="http://www.w3.org/2000/svg" width="' . $icon_size . '" height="' . $icon_size . '" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16"><path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"/></svg>';
+								break;
+							case 'pinterest':
+								$icon = '<svg xmlns="http://www.w3.org/2000/svg" width="' . $icon_size . '" height="' . $icon_size . '" fill="currentColor" class="bi bi-pinterest" viewBox="0 0 16 16"><path d="M8 0a8 8 0 0 0-2.915 15.452c-.07-.633-.134-1.606.027-2.297.146-.625.938-3.977.938-3.977s-.239-.479-.239-1.187c0-1.113.645-1.943 1.448-1.943.682 0 1.012.512 1.012 1.127 0 .686-.437 1.712-.663 2.663-.188.796.4 1.446 1.185 1.446 1.422 0 2.515-1.5 2.515-3.664 0-1.915-1.377-3.254-3.342-3.254-2.276 0-3.612 1.707-3.612 3.471 0 .688.265 1.425.595 1.826a.24.24 0 0 1 .056.23c-.061.252-.196.796-.222.907-.035.146-.116.177-.268.107-1-.465-1.624-1.926-1.624-3.1 0-2.523 1.834-4.84 5.286-4.84 2.775 0 4.932 1.977 4.932 4.62 0 2.757-1.739 4.976-4.151 4.976-.811 0-1.573-.421-1.834-.919l-.498 1.902c-.181.695-.669 1.566-.995 2.097A8 8 0 1 0 8 0"/></svg>';
+								break;
+							case 'youtube':
+								$icon = '<svg xmlns="http://www.w3.org/2000/svg" width="' . $icon_size . '" height="' . $icon_size . '" fill="currentColor" class="bi bi-youtube" viewBox="0 0 16 16"><path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408z"/></svg>';
+								break;
+							case 'twitch':
+								$icon = '<svg xmlns="http://www.w3.org/2000/svg" width="' . $icon_size . '" height="' . $icon_size . '" fill="currentColor" class="bi bi-twitch" viewBox="0 0 16 16"><path d="M3.857 0 1 2.857v10.286h3.429V16l2.857-2.857H9.57L14.714 8V0H3.857zm9.714 7.429-2.285 2.285H9l-2 2v-2H4.429V1.143h9.142z"/><path d="M11.857 3.143h-1.143V6.57h1.143zm-3.143 0H7.571V6.57h1.143z"/></svg>';
+								break;
+							case 'tiktok':
+								$icon = '<svg xmlns="http://www.w3.org/2000/svg" width="' . $icon_size . '" height="' . $icon_size . '" fill="currentColor" class="bi bi-tiktok" viewBox="0 0 16 16"><path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z"/></svg>';
+								break;
+							case 'threads':
+								$icon = '<svg xmlns="http://www.w3.org/2000/svg" width="' . $icon_size . '" height="' . $icon_size . '" fill="currentColor" class="bi bi-threads" viewBox="0 0 16 16"><path d="M6.321 6.016c-.27-.18-1.166-.802-1.166-.802.756-1.081 1.753-1.502 3.132-1.502.975 0 1.803.327 2.394.948.591.621.928 1.509 1.005 2.644.328.138.63.299.905.484 1.109.745 1.719 1.86 1.719 3.137 0 2.716-2.226 5.075-6.256 5.075C4.594 16 1 13.987 1 7.994 1 2.034 4.482 0 8.044 0 9.69 0 13.55.243 15 5.036l-1.36.353C12.516 1.974 10.163 1.43 8.006 1.43c-3.565 0-5.582 2.171-5.582 6.79 0 4.143 2.254 6.343 5.63 6.343 2.777 0 4.847-1.443 4.847-3.556 0-1.438-1.208-2.127-1.27-2.127-.236 1.234-.868 3.31-3.644 3.31-1.618 0-3.013-1.118-3.013-2.582 0-2.09 1.984-2.847 3.55-2.847.586 0 1.294.04 1.663.114 0-.637-.54-1.728-1.9-1.728-1.25 0-1.566.405-1.967.868ZM8.716 8.19c-2.04 0-2.304.87-2.304 1.416 0 .878 1.043 1.168 1.6 1.168 1.02 0 2.067-.282 2.232-2.423a6.217 6.217 0 0 0-1.528-.161"/></svg>';
+								break;
+							case 'bluesky':
+								$icon = '<svg xmlns="http://www.w3.org/2000/svg" width="' . $icon_size . '" height="' . $icon_size . '" fill="currentColor" class="bi bi-square-fill" viewBox="0 0 16 16"><path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2z"/></svg>';
+								break;
+							default:
+								$icon = '';
+								break;
+						}
+
+						$output .= ' <li>' . ( isset( $link['url'] ) ? ( ! empty( $link['url'] ) ? '<a target="_blank" href="' . $link['url'] . '">' : '' ) : '' ) . $icon . '<span class="visually-hidden-focusable"> ' . ucwords( $service ) . '</span>' . ( isset( $link['url'] ) ? ( ! empty( $link['url'] ) ? '</a>' : '' ) : '' ) . '</li>';
+					}
+
+					$output .= '</ul>';
+				}
+			}
+		}
+
+		return $output;
+	}
 
 // Register AJAX response
 
@@ -374,6 +440,56 @@ function method_people_grid_register_people_metabox() {
             'type'     => 'text',
         )
     );
+	$group_field_social_accounts = $cmb_options->add_field(
+		array(
+			'id'          => '_method_people_social_accounts',
+			'type'        => 'group',
+			'description' => __( 'Below, add relevant social accounts if opting to dispay them.', 'method-people-grid' ),
+			// 'repeatable'  => false, // use false if you want non-repeatable group
+			'options'     => array(
+				'group_title'       => __( 'Account {#}', 'method-people-grid' ), // since version 1.1.4, {#} gets replaced by row number
+				'add_button'        => __( 'Add Another Account', 'method-people-grid' ),
+				'remove_button'     => __( 'Remove Account', 'method-people-grid' ),
+				'sortable'          => true,
+				'closed'         => true, // true to have the groups closed by default
+				// 'remove_confirm' => esc_html__( 'Are you sure you want to remove?', 'method-people-grid' ), // Performs confirmation before removing group.
+			),
+		)
+	);
+
+	$cmb_options->add_group_field(
+		$group_field_social_accounts,
+		array(
+			'name' => 'Service',
+			'id'   => 'service',
+			'type' => 'select',
+			'show_option_none' => true,
+			'default' => '',
+			'desc' => __( 'Which service are you adding a link for?', 'method-people-grid' ),
+			'options' => array(
+				'facebook' => esc_attr__( 'Facebook', 'method-people-grid' ),
+				'twitter' => esc_attr__( 'Twitter (X)', 'method-people-grid' ),
+				'linkedin' => esc_attr__( 'LinkedIn', 'method-people-grid' ),
+				'instagram' => esc_attr__( 'Instagram', 'method-people-grid' ),
+				'pinterest' => esc_attr__( 'Pinterest', 'method-people-grid' ),
+				'youtube' => esc_attr__( 'YouTube', 'method-people-grid' ),
+				'twitch' => esc_attr__( 'Twitch', 'method-people-grid' ),
+				'tiktok' => esc_attr__( 'TikTok', 'method-people-grid' ),
+				'threads' => esc_attr__( 'Threads', 'method-people-grid' ),
+				'bluesky' => esc_attr__( 'Bluesky', 'method-people-grid' ),
+			),
+		)
+	);
+
+	$cmb_options->add_group_field(
+		$group_field_social_accounts,
+		array(
+			'name' => __( 'Profile URL', 'method-people-grid' ),
+			'desc' => __( 'Enter the full URL for your profile.', 'method-people-grid' ),
+			'id'   => 'url',
+			'type' => 'text_url',
+		)
+	);
 }
 
 add_action( 'cmb2_admin_init', 'method_people_grid_register_peoplegroup_metabox' );
@@ -528,4 +644,45 @@ function method_people_grid_register_plugin_options() {
         ),
         'default' => 'yes',
     ) );
+	$cmb_options->add_field( array(
+        'name'    => 'Default Social Link Inclusion',
+        'id'      => 'shortcode_social',
+        'type'    => 'radio',
+        'desc'    => 'By default, should social links be displayed for each person (if links have been provided)?',
+        'options' => array(
+            'yes' => __( 'Yes, show social links unless overridden by shortcode arguments.', 'method-people-grid' ),
+            'no'   => __( 'No, do not show social links unless overriden by shortcode arguments.', 'method-people-grid' ),
+        ),
+        'default' => 'no',
+    ) );
+}
+
+function method_people_grid_check_array_key( $item, $key ) {
+	$output = false;
+	if ( is_array( $item ) ) {
+		if ( array_key_exists( $key, $item ) ) {
+			if ( ! empty( $item["{$key}"] ) ) {
+				$output = true;
+			}
+		}
+	}
+	return $output;
+}
+
+	//-----------------------------------------------------
+	// Check to see if an array has content.
+	//-----------------------------------------------------
+
+function method_people_grid_check_array( $item, $key ) {
+	$output = false;
+	if ( $item ) {
+		if ( is_array( $item ) ) {
+			if ( 1 <=count( $item ) ) {
+				if ( method_people_grid_check_array_key( $item[0], $key ) ) {
+					$output = true;
+				}
+			}
+		}
+	}
+	return $output;
 }
